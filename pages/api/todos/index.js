137e6ab7402334a137702f1bbd4560852f01c931
MehrabD20 <mehrab20@gmail.com>
2023-07-08 10:14:09 +0330

    get and post actions refactored

--- pages/api/todos/index.js
+++ pages/api/todos/index.js
@@ -5,15 +5,13 @@ import Todo from "@/server/models/todo";
 dbConnect();
 
 export default async function handler(req, res) {
-   if (req.method === "POST") {
-      const newTodo = {
-         id: new Date().getTime(),
-         title: req.body.title,
-         isCompleted: false,
-      };
-      todos.push(newTodo);
+   const { method, body } = req;
+   if (method === "POST") {
+      const { title } = body;
+      await Todo.create({ title, isCompleted: false });
+      const todos = await Todo.find({});
       return res.status(201).json({ message: "new todo added!", todos });
-   } else if (req.method === "GET") {
+   } else if (method === "GET") {
       const todos = await Todo.find({});
       return res.status(200).json({
          todos,
